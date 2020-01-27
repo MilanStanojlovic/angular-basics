eefe350ebe422b51d58362bcb18f73d64bfbd22d
Milan <mistikone1@gmail.com>
2020-01-27 20:19:36 +0100

    directives and conditional outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  username: string = '';
+  showPassword: boolean = false;
+  log = [];
+
+  onResetUser = () => {
+    this.username = '';
+  }
+
+  onDisplay = () => {
+    this.showPassword = !this.showPassword;
+    this.log.push(this.log.length + 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -17,7 +18,8 @@ import { SuccessAlert } from './successAlert/successAlert.component';
     SuccessAlert,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
